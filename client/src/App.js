c56fcb71f428f5ef10a1458d397317e7884a1cb3
furfio <1722941803@1722941803@qq.com>
2021-07-16 22:09:32 +0800

    count

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
+
+import ReconnectingWebSocket from 'reconnecting-websocket';
+const sharedb = require('sharedb/lib/client');
+
+// Open WebSocket connection to ShareDB server
+const socket = new ReconnectingWebSocket("ws://localhost:8080");
+const connection = new sharedb.Connection(socket);
+
+// Create local Doc instance mapped to 'examples' collection document with id 'counter'
+const doc = connection.get('examples', 'counter');
+
+function increment() {
+  // Increment `doc.data.numClicks`. See
+  // https://github.com/ottypes/json0 for list of valid operations.
+  doc.submitOp([{p: ['numClicks'], na: 1}]);
+}
+
+const showNumbers =()=>{
+  document.querySelector('#click').textContent = doc.data.numClicks;
+}
 
 function App() {
+
+  useEffect(()=>{
+    // Get initial value of document and subscribe to changes
+    doc.subscribe(showNumbers);
+    // When document changes (by this client or any other, or the server),
+    // update the number on the page
+    doc.on('op', showNumbers);
+  },[connection])
   return (
     <div className="App">
-        hi
+      <span id="click"></span>
+      <button onClick={increment}> +1</button>
     </div>
   );
 }

72c889b5c3bbeb17b2e82e551d3281e10858ba88
furfio <1722941803@1722941803@qq.com>
2021-07-18 23:49:31 +0800

    fabric

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState,useEffect} from 'react'
+import { fabric } from 'fabric'
 
 import ReconnectingWebSocket from 'reconnecting-websocket';
 const sharedb = require('sharedb/lib/client');
@@ -11,17 +12,29 @@ const connection = new sharedb.Connection(socket);
 // Create local Doc instance mapped to 'examples' collection document with id 'counter'
 const doc = connection.get('examples', 'counter');
 
-function increment() {
-  // Increment `doc.data.numClicks`. See
-  // https://github.com/ottypes/json0 for list of valid operations.
-  doc.submitOp([{p: ['numClicks'], na: 1}]);
-}
+let rect = new fabric.Rect({
+  left: 100,
+  top: 50,
+  fill: 'yellow',
+  width: 200,
+  height: 100,
+  // objectCaching: false,
+  // stroke: 'lightgreen',
+  // strokeWidth: 4,
+});
 
-const showNumbers =()=>{
-  document.querySelector('#click').textContent = doc.data.numClicks;
-}
+
 
 function App() {
+  const [num,setNum] = useState()
+  const [canvas, setCanvas] = useState('');
+  const initCanvas = () => (
+      new fabric.Canvas('canvas', {
+        height: 800,
+        width: 800,
+        backgroundColor: 'pink'
+      })
+  );
 
   useEffect(()=>{
     // Get initial value of document and subscribe to changes
@@ -29,11 +42,29 @@ function App() {
     // When document changes (by this client or any other, or the server),
     // update the number on the page
     doc.on('op', showNumbers);
+
+    setCanvas(initCanvas());
+
   },[connection])
+
+  function increment() {
+    // Increment `doc.data.numClicks`. See
+    // https://github.com/ottypes/json0 for list of valid operations.
+    doc.submitOp([{p: ['numClicks'], na: 1}]);
+    canvas.add(rect)
+  }
+  const showNumbers =()=>{
+    setNum(doc.data.numClicks)
+    document.querySelector('#click').textContent = doc.data.numClicks;
+  }
+
   return (
     <div className="App">
       <span id="click"></span>
       <button onClick={increment}> +1</button>
+      {num}
+      <br></br>
+      <canvas id='canvas' style={{border:'solid'}}></canvas>
     </div>
   );
 }
